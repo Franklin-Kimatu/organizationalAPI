0fc2c393a9d3838097d3522c789aa3562b9a3a6e
Franklin-Kimatu <frankimatu@gmail.com>
2019-09-23 18:48:42 +0300

    add the routes for getting and posting the API entries from the database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -30,5 +30,19 @@ public class App {
             response.type("application/json");
             return gson.toJson(department);
         });
+
+        get("/departments","application/json",(request, response) -> {
+            response.type("application/json");
+            return gson.toJson(departmentDao.getAll());
+        });
+
+        get("/departments/:id","application/json",(request, response) -> {
+            response.type("application/json");
+            int departmentId =Integer.parseInt(request.params("id"));
+            response.type("application/json");
+            return gson.toJson(departmentDao.findById(departmentId));
+        });
+
+
     }
 }
